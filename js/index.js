0d569b75e6dd56dceb4eec5dca24916ccc37331f
zhangxinxin <1285367184@qq.com>
2023-05-11 17:23:53 +0800

    错误演示功能完成

--- js/index.js
+++ js/index.js
@@ -1,90 +1,42 @@
-let oldPosition = '1-4';
+let oldPosition = '1';
 let currentPosition = '2-1';
+let value = '';
 
 const inputEl = document.getElementById('search-input');
-const tds = document.querySelectorAll('td');
+const spans = document.querySelectorAll('span');
 const tdItems = {};
 
-const key = [
-  {
-    name: 't1',
-  },
-  {
-    name: 't2',
-  },
-  {
-    name: 't3',
-  },
-  {
-    name: 't4',
-  },
-  {
-    name: 't5',
-  },
-  {
-    name: 't6',
-  },
-  {
-    name: 't7',
-  },
-  {
-    name: 't8',
-  },
-  {
-    name: 't9',
-  },
-  {
-    name: 't10',
-  },
-];
-
-for (let i = 0; i < key.length; i++) {
-  if (i === 0) {
-    tds[0].className = key[i].name;
-  } else if (i === 1) {
-    tds[1].className = key[i - 1].name;
-  } else if (i === 2) {
-    tds[2].className = key[i - 1].name;
-  } else if (i === 3) {
-    tds[3].className = key[i - 1].name;
-  } else if (i === 4) {
-    tds[4].className = key[i - 1].name;
-  } else if (i === 5) {
-    tds[5].className = key[i - 1].name;
-  } else if (i === 6) {
-    tds[6].className = key[i - 1].name;
-  } else if (i === 7) {
-    tds[7].className = key[i - 1].name;
-  } else if (i === 8) {
-    tds[8].className = key[i - 1].name;
-  } else if (i === 9) {
-    tds[9].className = key[i - 1].name;
-  }
-}
-
-for (let td of tds) {
-  tdItems[td.id] = td;
+for (let span of spans) {
+  tdItems[span.id] = span;
 }
 
 document.getElementById('search-btn').addEventListener('click', () => {
-  // debugger;
-  for (let i = 0; i < tds.length; i++) {
-    if (tds[i].classList.value === inputEl.value) {
-      console.log(tds[i].classList.value);
-      console.log(tds[i]);
-    }
+  // for (let i = 0; i < spans.length; i++) {
+  //   if (spans[i].classList.value === inputEl.value) {
+  //     console.log(spans[i]);
+  //     break;
+  //   }
+  // }
+
+  for (let id in tdItems) {
+    tdItems[id].className = '';
   }
 
+  value = inputEl.value;
+
   $.ajax({
-    url: 'http://123.57.109.30:3006/api/getbooks',
+    url: 'http://192.168.100.95:8082/zhier/query',
+    data: {
+      code: value,
+    },
     type: 'get',
     dataType: 'json',
     async: false,
-    success: (res) => console.log(res),
+    success: (res) => {
+      oldPosition = res.data.oldLocation;
+      currentPosition = res.data.nowLocation;
+    },
   });
-  oldPosition = '2-5';
-  currentPosition = '1-1';
-
-  tdItems['line' + oldPosition].classList.add('blue');
-  tdItems['line' + currentPosition].classList.add('red');
+  tdItems[oldPosition].classList.add('blue');
+  tdItems[currentPosition].classList.add('red');
 });
