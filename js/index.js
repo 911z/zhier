ad859ca1b3df3b3e63e69233e73cfc6b1ad615bf
zhangxinxin <1285367184@qq.com>
2023-05-12 09:23:11 +0800

    页面搭建完成

--- js/index.js
+++ js/index.js
@@ -5,38 +5,42 @@ let value = '';
 const inputEl = document.getElementById('search-input');
 const spans = document.querySelectorAll('span');
 const tdItems = {};
+const containerEl = document.getElementsByClassName('container');
+const goodsInfoEl = document.getElementsByClassName('goods-info-container');
 
 for (let span of spans) {
   tdItems[span.id] = span;
 }
 
 document.getElementById('search-btn').addEventListener('click', () => {
-  // for (let i = 0; i < spans.length; i++) {
-  //   if (spans[i].classList.value === inputEl.value) {
-  //     console.log(spans[i]);
-  //     break;
-  //   }
-  // }
+  goodsInfoEl[0].classList.add('isHidden');
+  containerEl[0].classList.remove('isHidden');
 
   for (let id in tdItems) {
     tdItems[id].className = '';
   }
 
-  value = inputEl.value;
+  // value = inputEl.value;
 
-  $.ajax({
-    url: 'http://192.168.100.95:8082/zhier/query',
-    data: {
-      code: value,
-    },
-    type: 'get',
-    dataType: 'json',
-    async: false,
-    success: (res) => {
-      oldPosition = res.data.oldLocation;
-      currentPosition = res.data.nowLocation;
-    },
-  });
-  tdItems[oldPosition].classList.add('blue');
-  tdItems[currentPosition].classList.add('red');
+  // $.ajax({
+  //   url: 'http://192.168.100.95:8082/zhier/query',
+  //   data: {
+  //     code: 'A1001',
+  //   },
+  //   type: 'get',
+  //   dataType: 'json',
+  //   async: false,
+  //   success: (res) => {
+  //     oldPosition = res.data.oldLocation;
+  //     currentPosition = res.data.nowLocation;
+  //   },
+  // });
+  // tdItems[oldPosition].classList.add('blue');
+  // tdItems[currentPosition].classList.add('red');
+});
+
+document.getElementById('back-btn').addEventListener('click', () => {
+  console.log(1);
+  containerEl[0].classList.add('isHidden');
+  goodsInfoEl[0].classList.remove('isHidden');
 });
